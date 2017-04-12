2c4a1ab51aef4e2aabc816ef23b32b4f60a77581
felix <lecauchemarxy@gmail.com>
2017-04-12 10:00:52 +0200

    script changed

--- src/test.py
+++ src/test.py
@@ -2,11 +2,11 @@ from GeekManager import *
 
 gm = GeekManager()
 
-# gm.ReadNamesFromRawFile("../data/swcz.names")
-gm.ReadNamesIDsCSVFile("../data/swcz.namesids.csv")
-# gm.LoadIDsFromNames()
+gm.ReadNamesFromRawFile("../data/swcz.names")
+# gm.ReadNamesIDsCSVFile("../data/swcz.namesids.csv")
+gm.LoadIDsFromNames()
 #gm.ReadNamesIDsCSVFile("../data/namesids_spielenacht2016.csv")
-gm.RequestDetailsBatch(100)
+# gm.RequestDetailsBatch(100)
 # gm.PrintGames()
 # gm.WriteCSVFile("../data/out.csv")
-gm.WriteJSONFile("../html/swcz.json")
+# gm.WriteJSONFile("../html/swcz.json")
