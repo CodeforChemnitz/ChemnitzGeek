3d37cdacbbc3159fbe3f225657500239a7b2da76
felix <lecauchemarxy@gmail.com>
2017-04-13 17:52:15 +0200

    fix bib filename

--- html/logic.js
+++ html/logic.js
@@ -86,7 +86,7 @@ function addGameToLoadedGames(game, loadedCollIdx) {
   var id = game.bggID;
   var idExists = false;
   for (var i = 0; i < loadedGames.length; i++) {
-    if (loadedGames[i].bggID == id) {
+    if (loadedGames[i].bggID == id && !$.inArray(loadedCollIdx, loadedGames[i].loadedColls)) {
       loadedGames[i].loadedColls.push(loadedCollIdx);
       idExists = true;
       break;
